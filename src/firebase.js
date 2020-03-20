0156d5bed24ec4bcc3f94868609c5b7e985f89d5
rehan-sattar <rehansattar117@gmail.com>
2020-03-21 03:33:52 +0500

    fix(firebase) - add process env variables to config

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import 'firebase/database'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: REACT_APP_apiKey,
-  authDomain: authDomain,
-  databaseURL: REACT_APP_databaseURL,
-  projectId: REACT_APP_projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: REACT_APP_messagingSenderId,
-  appId: REACT_APP_appId,
-  measurementId: REACT_APP_measurementId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
-firebase.analytics()
+
+export default firebase
