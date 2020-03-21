72dd703f51f4ad7c28f1c837e8bc61dc0be5bd01
rehan-sattar <rehansattar117@gmail.com>
2020-03-21 15:14:49 +0500

    feat(login) - add login form & functionality

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,124 @@
 import React from 'react'
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import firebase from '../../firebase'
 
 export default function Login() {
-  return <div>Login</div>
+  const initialState = {
+    email: '',
+    password: '',
+  }
+
+  const [loginUserState, setRegisterUserState] = React.useState(initialState)
+  const [errors, setErrors] = React.useState([])
+  const [status, setStatus] = React.useState('')
+
+  const handleChange = event => {
+    setRegisterUserState({
+      ...loginUserState,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const onSubmit = async event => {
+    event.preventDefault()
+    const errors = []
+    if (formIsValid(loginUserState)) {
+      const { email, password } = loginUserState
+      setErrors(errors)
+      setStatus('PENDING')
+
+      try {
+        const signedUser = await firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+        console.log('SIGNED USER: ', signedUser)
+        setStatus('RESOLVED')
+      } catch (err) {
+        setStatus('RESOLVED')
+        setErrors(errors.concat({ message: err.message }))
+      }
+    } else {
+      let error = { message: 'Please fill the form' }
+      setErrors(errors.concat(error))
+    }
+  }
+
+  const formIsValid = ({ email, password }) => {
+    return email && password
+  }
+
+  const handleInputError = (errors, inputName) => {
+    return errors.some(err =>
+      err.message.toLowerCase().includes(inputName.toLowerCase())
+    )
+      ? 'error'
+      : ''
+  }
+
+  const { email, password } = loginUserState
+
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className="app">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="purple" textAlign="center">
+          <Icon name="puzzle piece" color="purple" />
+          Login for slack clone!!
+        </Header>
+        <Form size="large" onSubmit={onSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              type="email"
+              name="email"
+              icon="mail"
+              iconPosition="left"
+              placeholder="Email"
+              value={email}
+              onChange={handleChange}
+              className={handleInputError(errors, 'email')}
+            />
+            <Form.Input
+              fluid
+              type="password"
+              name="password"
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              value={password}
+              onChange={handleChange}
+              className={handleInputError(errors, 'password')}
+            />
+            <Button
+              disabled={status === 'PENDING'}
+              className={status === 'PENDING' ? 'loading' : ''}
+              type="submit"
+              fluid
+              color="purple"
+              size="large"
+            >
+              Submit
+            </Button>
+          </Segment>
+        </Form>
+        {errors.length > 0 &&
+          errors.map((err, i) => (
+            <Message error key={i}>
+              <p>{err.message}</p>
+            </Message>
+          ))}
+        <Message>
+          Don't have an account? <Link to="/register">Register</Link>{' '}
+        </Message>
+      </Grid.Column>
+    </Grid>
+  )
 }
