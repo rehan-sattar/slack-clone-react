f222707315438de8c0c80ef96bb0c39ceec9add8
rehan-sattar <rehansattar117@gmail.com>
2020-03-28 17:01:15 +0500

    feat(directMessage) - add user stastus & render all users

---
+++ src/components/SidePanel/DirectMessages.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+
+import firebase from '../../firebase'
+
+export default function DirectMessages({ currentUser }) {
+  const [users, setUsers] = useState([])
+  const [userRef] = useState(firebase.database().ref('users'))
+  const [connectedRef] = useState(firebase.database().ref('.info/connected'))
+  const [presenceRef] = useState(firebase.database().ref('presence'))
+
+  useEffect(() => {
+    if (currentUser) {
+      addListeners(currentUser.uid)
+    }
+  }, [])
+
+  /**
+   * Listener for getting all the users
+   */
+  const addListeners = currentUserId => {
+    userRef.on('child_added', snap => {
+      let user = snap.val()
+      user['uid'] = snap.key
+      user['status'] = 'offline'
+      setUsers(users => [...users, user])
+    })
+    connectedRef.on('value', snap => {
+      if (snap.val() === true) {
+        const ref = presenceRef.child(currentUserId)
+        ref.set(true)
+        ref.onDisconnect().remove(err => {
+          if (err !== null) {
+            console.log(err)
+          }
+        })
+        ref.on('child_added', snap => {
+          if (currentUserId === snap.key) {
+            setUserStatues(snap.key)
+          }
+        })
+
+        ref.on('child_removed', snap => {
+          if (currentUserId === snap.key) {
+            setUserStatues(snap.key, false)
+          }
+        })
+      }
+    })
+  }
+
+  const setUserStatues = (userId, connected = true) => {
+    const updatedUsers = users.reduce((acc, user) => {
+      if (user.uid === userId) {
+        user['status'] = `${connected ? 'online' : 'offline'}`
+      }
+      return acc.concat(user)
+    }, [])
+
+    setUsers(updatedUsers)
+  }
+
+  const isUserOnline = user => user.status === 'online'
+
+  return (
+    <Menu.Menu>
+      <Menu.Item>
+        <span>
+          <Icon name="mail" /> Direct Messages
+        </span>{' '}
+        (2) <Icon name="add" onClick={() => {}} />
+      </Menu.Item>
+      {users.map((user, index) => {
+        return (
+          <Menu.Item
+            key={user.uid}
+            onClick={() => console.log(user)}
+            style={{ opacity: 0.7, fontStyle: 'italic' }}
+          >
+            <Icon name="circle" color={isUserOnline(user) ? 'green' : 'red'} />@{' '}
+            {user.name}
+          </Menu.Item>
+        )
+      })}
+    </Menu.Menu>
+  )
+}
--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -3,6 +3,7 @@ import { Menu } from 'semantic-ui-react'
 
 import UserPanel from './UserPanel'
 import Channels from './Channels'
+import DirectMessages from './DirectMessages'
 
 export default function SidePanel({ currentUser }) {
   return (
@@ -16,6 +17,7 @@ export default function SidePanel({ currentUser }) {
     >
       <UserPanel currentUser={currentUser} />
       <Channels />
+      <DirectMessages currentUser={currentUser} />
     </Menu>
   )
 }
